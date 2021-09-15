476357f2d52d3f4fefa42139c13ed0518c9068b6
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-09-15 16:41:45 -0700

    generated model

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,11 +11,11 @@ import (
 	"github.com/tclohm/linkdrop/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+func (r *queryResolver) Link(ctx context.Context, url string) (*model.Link, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
